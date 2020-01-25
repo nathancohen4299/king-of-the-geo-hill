386c3000103cba8823001fa3300dadb216deef4e
Ryan Chen <ryanjchen2@gmail.com>
2020-01-24 23:24:31 -0500

    fix

--- api/app.py
+++ api/app.py
@@ -2,14 +2,14 @@ from flask import Flask, jsonify
 import requests
 from google.cloud import firestore
 
-from utils.auth import auth
+import utils.auth as auth
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hello"
+    return jsonify(200)
 
 if __name__=="__main__":
     app.run()
