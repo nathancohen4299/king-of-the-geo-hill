cb20d46351fc4fc8c7ae7ee018ef04c54f2f3df9
Ryan Chen <ryanjchen2@gmail.com>
2020-01-25 00:55:22 -0500

    added routes

--- api/app.py
+++ api/app.py
@@ -2,14 +2,31 @@ from flask import Flask, jsonify
 import requests
 from google.cloud import firestore
 
-import utils.auth as auth
-
+from utils.auth import authenticate
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return jsonify(200)
+
+@app.route("/user", methods=["POST", "GET"])
+def user_route():
+    if request.method == "POST":
+        create_user(request.body)
+    elif request.method == "GET":
+        get_user(request.body)
+
+@app.route("/game", method=["POST", "GET"])
+def game_route():
+    if request.method == "POST":
+        create_game(request.body)
+    elif request.method == "GET":
+        get_game(request.body)
+
+@app.route("/game/score", method="GET")
+def score_route():
+    return jsonify(get_score)
 
 if __name__=="__main__":
     app.run()
