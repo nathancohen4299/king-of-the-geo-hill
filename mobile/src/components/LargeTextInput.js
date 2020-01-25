9bc2781ebe124e6519e46ece58cce63ab51e1790
Stephen Davis <davi1138@purdue.edu>
2020-01-25 14:27:19 -0500

    implemented create game functionality

--- mobile/src/components/LargeTextInput.js
+++ mobile/src/components/LargeTextInput.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { TextInput } from 'react-native'
+import { TextInputMask } from 'react-native-masked-text'
 
 export const LargeTextInput = ({text, onChangeText = () => {}, placeholder}) => {
     return (
@@ -8,6 +9,18 @@ export const LargeTextInput = ({text, onChangeText = () => {}, placeholder}) => 
             onChangeText={text => onChangeText(text)}
             value={text}
             placeholder={placeholder}
+        />
+    )
+}
+
+export const LargeNumericTextInput = ({text, onChangeText = () => {}, placeholder}) => {
+    return (
+        <TextInputMask
+            style={{ height: 64, borderColor: 'gray', borderWidth: 2, borderRadius: 4, marginHorizontal: '15%', fontSize: 24, fontFamily: 'Bangers-Regular', textAlign: 'center'}}
+            onChangeText={text => onChangeText(text)}
+            value={text}
+            placeholder={placeholder}
+            type={'only-numbers'}
         />
     )
 }
