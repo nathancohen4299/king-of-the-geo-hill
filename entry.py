039a09d80d6fabf15800fcc1bebade722f30241b
Ryan Chen <ryanjchen2@gmail.com>
2020-01-25 20:57:33 -0500

    Took out duplicate line

--- entry.py
+++ entry.py
@@ -6,11 +6,11 @@ from api.app import app
 class Config(object):
     JOBS = [
         {
-            'id': 'job1',
-            'func': 'entry:job1',
-            'args': (1, 2),
-            'trigger': 'interval',
-            'seconds': 1
+            "id": "job1",
+            "func": "entry:job1",
+            "args": (1, 2),
+            "trigger": "interval",
+            "seconds": 1,
         }
     ]
 
@@ -18,8 +18,11 @@ class Config(object):
 
 
 def job1(a, b):
-    headers = {'Authorization': 'prj_test_sk_593c83bc7be1078df3fd09f125eb776f96906dee'}
-    r = requests.get("https://api.radar.io/v1/geofences/5e2c9c6e5f526200f02d9cb4/users", headers=headers)
+    headers = {"Authorization": "prj_test_sk_593c83bc7be1078df3fd09f125eb776f96906dee"}
+    r = requests.get(
+        "https://api.radar.io/v1/geofences/5e2c9c6e5f526200f02d9cb4/users",
+        headers=headers,
+    )
     app.logger.critical(r.text)
 
 
@@ -32,5 +35,4 @@ if __name__ == "__main__":
     scheduler.init_app(app)
     scheduler.start()
 
-    app.run()
     app.run()
