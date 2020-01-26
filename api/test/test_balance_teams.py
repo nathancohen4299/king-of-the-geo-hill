35a432027ab8b2c8dd6879c2e74eee3c0280fe39
nathancohen4299 <ncohen4299@gmail.com>
2020-01-26 01:09:03 -0500

    Updated testcases

--- api/test/test_balance_teams.py
+++ api/test/test_balance_teams.py
@@ -29,3 +29,12 @@ def test_team_balance_3():
     list3 = [5]
     Game.balance_teams(list1, list2, list3)
     assert balanced(list1, list2)
+
+def test_team_balance_4():
+    list1 = [1, 2, 3, 4, 5]
+    list2 = [6, 7, 3]
+    list3 = []
+    list1_len_before = len(list1)
+    list2_len_before = len(list2)
+    Game.balance_teams(list1, list2, list3)
+    assert len(list1) == list1_len_before and len(list2) == list2_len_before
